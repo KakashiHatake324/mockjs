2cf458caba383dadc2db66c296fbe56ca97d934a
rafaeltorres324@gmail.com <rafaeltorres324@gmail.com>
2024-06-20 09:24:09 -0400

    changed to crypto/rand instead of math/rand

--- utils.go
+++ utils.go
@@ -1,21 +1,49 @@
 package mockjs
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"math/big"
 )
 
 func Random_range(min, max int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max-min+1) + min
+	rangeVal := max - min + 1
+	if rangeVal <= 0 {
+		return 0
+	}
+
+	// Generate a random number in the range [0, rangeVal)
+	n, err := rand.Int(rand.Reader, big.NewInt(int64(rangeVal)))
+	if err != nil {
+		return 0
+	}
+
+	return min + int(n.Int64())
 }
 
 func RandomFloat() float64 {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Float64()
+	numerator, err := rand.Int(rand.Reader, big.NewInt(1<<53))
+	if err != nil {
+		return 0
+	}
+	denominator := float64(1 << 53)
+	randomFloat := float64(numerator.Int64()) / denominator
+
+	return randomFloat
 }
 
-func Random_Float_Range(minimum, maximum float64) float64 {
-	rand.Seed(time.Now().UnixNano())
-	return (rand.Float64() * (maximum - minimum)) + minimum
+func Random_Float_Range(min, max float64) float64 {
+	if min >= max {
+		return 0
+	}
+	// Generate a random number between 0 and 1
+	numerator, err := rand.Int(rand.Reader, big.NewInt(1<<53))
+	if err != nil {
+		return 0
+	}
+
+	denominator := float64(1 << 53)
+	randomFloat := float64(numerator.Int64()) / denominator
+
+	// Scale to the desired range
+	return min + randomFloat*(max-min)
 }
